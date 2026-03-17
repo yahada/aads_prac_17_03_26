39bdc209d4654ae4d921ea4a69663faa0ac07437
yahada <niadzy14@gmail.com>
2026-03-17 11:12:07 +0300

    test framework

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include "top-it-vector.hpp"
+#include <iostream>
+#include <iomanip>
+#include <utility>
 
+bool testEmptyVector()
+{
+  return false;
+}
 
 int main()
-{}
+{
+  using test_t = std::pair< const char *, bool(*)() > ;
+  test_t tests[] = {
+    { "Empty vector", testEmptyVector}
+  };
+  const size_t count = sizeof(tests) / sizeof(test_t);
+  bool pass = true;
+  for (size_t i = 0; i < count; ++i)
+  {
+    std::cout << std::boolalpha;
+    bool res = tests[i].second();
+    std::cout << tests[i].first << ": " << tests[i].second() << '\n';
+    pass = pass && res;
+  }
+  std::cout << "RESULT: " << pass << '\n';
+}
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -1,9 +1,34 @@
 #ifndef TOP_IT_VECTOR_HPP
 #define TOP_IT_VECTOR_HPP
 #include <cstddef>
+namespace topit {
+  template< class T >
+  struct Vector {
+  public:
+    Vector();
+    ~Vector();
+
+
+  private:
+    T* data_;
+    size_t size_, capacity_;
+  };
+}
+
 template< class T >
-struct Vector {
-  T* data;
-  size_t size, capacity;
-};
+topit::Vector< T >::~Vector()
+{
+
+}
+
+template< class T >
+topit::Vector< T >::Vector():
+  data_(),
+  size_(),
+  capacity_()
+{
+  
+}
+
+
 #endif
