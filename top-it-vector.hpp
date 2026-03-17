e6dd03ded3844dc0dce34dec7309ed422c0a1c0f
yahada <niadzy14@gmail.com>
2026-03-17 14:37:37 +0300

    end of class work

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -6,10 +6,23 @@ namespace topit
   template< class T >
   struct Vector {
   public:
-    Vector();
     ~Vector();
-    bool isEmpty() const noexcept;
 
+    Vector();
+    Vector(const Vector&);
+    Vector(Vector&&);
+    Vector& operator=(const Vector&);
+    Vector& operator=(Vector&&);
+
+
+    bool isEmpty() const noexcept;
+    size_t getSize() const noexcept;
+    size_t getCapacity() const noexcept;
+
+    void pushBack(const T& v);
+    void popBack();
+    void insert(size_t i, const T& v);
+    void erase(size_t i);
 
   private:
     T* data_;
@@ -20,17 +33,24 @@ namespace topit
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept
 {
-  return false;
+  return !size_;
+}
+template< class T >
+void topit::Vector< T >::pushBack(const T& v)
+{
+
 }
 
 template< class T >
 topit::Vector< T >::~Vector()
-{}
+{
+  delete[] data_;
+}
 
 template< class T >
 topit::Vector< T >::Vector():
-  data_(),
-  size_(),
-  capacity_()
+  data_(nullptr),
+  size_(0),
+  capacity_(0)
 {}
 #endif
